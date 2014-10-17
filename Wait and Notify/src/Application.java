6833d35b9b543d9194ffac8b1d5de6146c93e441
amankhatri <aman_khatri5b5@yahoo.com>
2014-10-16 22:11:32 -0500

    notify wait producer consumer threads

---
+++ Wait and Notify/src/Application.java
@@ -0,0 +1,48 @@
+/*Learning about Producer Consumer*/
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Processor processor = new Processor();
+		Thread t1 = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				try {
+					processor.produce();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			
+		});
+		Thread t2 = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					processor.consumer();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			
+		});
+		t1.start();
+		t2.start();
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
